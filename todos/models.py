83e1a58efde81c8f3ddb140a26c8ee53a1e32c7f
Holman Gao <holman@golmansax.com>
2015-05-08 18:34:47 -0700

    Add model

--- todos/models.py
+++ todos/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Todo(models.Model)
+    name = models.CharField(max_length=200)
+    due_date = models.DateTimeField('due date')
+    completed_date = models.DateTimeField('completed date')
