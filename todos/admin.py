e486c5a791eacb5bd3852bb75d4aad5008560b0c
Holman Gao <holman@golmansax.com>
2015-05-09 10:04:55 -0700

    Adding in TodoAdmin

--- todos/admin.py
+++ todos/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Todo
+
+class TodoAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['name']}),
+        ('Date information', {'fields': ['completed_date', 'due_date'], 'classes': ['collapse']})
+    ]
+
+admin.site.register(Todo, TodoAdmin)
